3b3fed783cb1056b2e2284f78b0a259760396274
Thomas Haederle <thomas.haederle@gmail.com>
2026-07-01 13:19:34 +0100

    Add result-store hierarchy query APIs

--- packages/frtb-result-store/src/frtb_result_store/org_hierarchy.py
+++ packages/frtb-result-store/src/frtb_result_store/org_hierarchy.py
@@ -19,6 +19,19 @@ from frtb_result_store.org_hierarchy_model import (
     OrgSliceKeys,
     generate_org_aggregate_row_id,
 )
+from frtb_result_store.org_hierarchy_queries import (
+    aggregate_org_node,
+    list_org_hierarchy,
+    org_node_children,
+    source_rows_for_org_node,
+)
+from frtb_result_store.org_hierarchy_query_model import (
+    OrgHierarchyEdge,
+    OrgHierarchySnapshot,
+    OrgNodeAggregateResult,
+    OrgQueryStatus,
+    OrgSourceRowPage,
+)
 from frtb_result_store.org_hierarchy_validation import (
     resolve_org_hierarchy_version,
     validate_org_hierarchy,
@@ -28,14 +41,23 @@ __all__ = [
     "OrgAggregateRow",
     "OrgCapitalResultRow",
     "OrgHierarchy",
+    "OrgHierarchyEdge",
     "OrgHierarchyLevel",
     "OrgHierarchyNode",
+    "OrgHierarchySnapshot",
+    "OrgNodeAggregateResult",
+    "OrgQueryStatus",
     "OrgSliceKeys",
+    "OrgSourceRowPage",
     "aggregate_by_org_hierarchy",
+    "aggregate_org_node",
     "generate_org_aggregate_row_id",
+    "list_org_hierarchy",
+    "org_node_children",
     "resolve_org_hierarchy_version",
     "sample_org_capital_rows",
     "sample_org_hierarchy",
     "source_rows_for_org_aggregate",
+    "source_rows_for_org_node",
     "validate_org_hierarchy",
 ]
